5ea93086de8585b10ee9d52ba0491e95865b1647
dode2511 <leondodejr@gmail.com>
2023-12-17 17:39:56 -0300

    a

--- components/Grafic/Categoria.jsx
+++ components/Grafic/Categoria.jsx
@@ -10,51 +10,42 @@ function Categoria() {
 
 
 
+
   useEffect(() => {
     if (usuarioId) {
       console.log('Usuário ID:', usuarioId);
       // Lógica adicional que depende de usuarioId
     }
+    
+    async function getDadosGrafico() {
+      try {
+        if (!usuarioId) {
+          // Se usuarioId ainda não estiver definido, saia da função
+          return;
+        }
+
+        const response = await fetch(`https://api-conta-certa-production.up.railway.app/graphSaidas/${usuarioId}?mes=11&ano=2023`);
+        
+        if (!response.ok) {
+          // Se a resposta não estiver ok, trate o erro aqui
+          console.error('Erro ao obter dados:', response.statusText);
+          return;
+        }
+
+        const dados = await response.json();
+        console.log(dados);
+        setDadosDoBanco(dados);
+      } catch (error) {
+        console.error('Erro ao buscar dados da API:', error);
+      }
+    }
+
+    getDadosGrafico();
   }, [usuarioId]);
 
- 
-
-
+  
   const [dadosDoBanco, setDadosDoBanco] = useState([]);
 
-  useEffect(() => {
-    fetch(`http://localhost:54168/graphSaidas/${usuarioId}?mes=11&ano=2023`)
-      .then(response => response.json())
-      .then(data => setDadosDoBanco(data))
-      .catch(error => console.error('Erro ao buscar dados da API:', error));
-  }, []);
-
-
-
-  const data = {
-    labels: dadosDoBanco.map(item => item.categoria),
-    datasets: [{
-      label: '',
-      data: dadosDoBanco.map(item => item.num),
-      backgroundColor: [
-        'rgba(22, 191, 214, 1)',
-        'rgba(179, 247, 238, 1)',
-      ],
-      hoverOffset: 4
-    }]
-  };
-
-  const config = {
-    type: 'pie',
-    data: data,
-    options: {
-      plugins: {
-        legend: {
-          position: 'bottom',
-        },
-      },
-    },
-  };
 
   const chartRef = useRef();
   const myChart = useRef(null);
@@ -62,16 +53,44 @@ function Categoria() {
   useEffect(() => {
     const ctx = chartRef.current.getContext('2d');
 
-    if (myChart.current) {
-      myChart.current.destroy();
-    }
+    // Verifica se há dados antes de criar o gráfico
+    if (dadosDoBanco.length > 0) {
+      const data = {
+        labels: dadosDoBanco.map(item => item.categoria),
+        datasets: [{
+          label: '',
+          data: dadosDoBanco.map(item => item.num),
+          backgroundColor: [
+            'rgba(22, 191, 214, 1)',
+            'rgba(179, 247, 238, 1)',
+          ],
+          hoverOffset: 4
+        }]
+      };
 
-    myChart.current = new Chart(ctx, config);
-  }, []);
+      const config = {
+        type: 'pie',
+        data: data,
+        options: {
+          plugins: {
+            legend: {
+              position: 'bottom',
+            },
+          },
+        },
+      };
+
+      if (myChart.current) {
+        myChart.current.destroy();
+      }
+
+      myChart.current = new Chart(ctx, config);
+    }
+  }, [dadosDoBanco]);
 
   return (
     <div>
-        <h1 style={{ fontSize: '14px', marginTop:'20px', marginBottom:'0', fontWeight:'800'}}>Despesas por categoria</h1>
+      <h1 style={{ fontSize: '14px', marginTop:'20px', marginBottom:'0', fontWeight:'800'}}>Despesas por categoria</h1>
       <canvas ref={chartRef} style={{ padding:'50px 50px' , marginBottom: '20px' }}></canvas>
     </div>
   );
