4d7512a337bd6f3aa4d391153ab43bb7529dbf01
Pedro Menna <113869595+pedromenna@users.noreply.github.com>
2023-12-17 22:16:51 -0300

    graficos-proximos-categoria

--- components/Grafic/Proximos.jsx
+++ components/Grafic/Proximos.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { format } from "date-fns";
+import { ptBR } from 'date-fns/locale';
 import "bootstrap/dist/css/bootstrap.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import styles from './Ultimas.module.css';
 
 function Proximos() {
     const [indiceAtual, setIndiceAtual] = useState(0);
-
-    const dadosDaTabela = [
-        { col1: "Água", col2: "18/ago", col3: "R$700,00"  },
-        { col1: "Aluguel", col2: "18/ago", col3: "R$900,00" },
-        { col1: "Luz", col2: "19/ago", col3: "R$600,00" },
-        { col1: "Lixo", col2: "20/ago", col3: "R$100,00"},
-        { col1: "Limpeza", col2: "20/ago", col3: "R$300,00"}
-    ];
+    const [dadosDaTabela, setDadosDaTabela] = useState([]);
 
     const tamanhoPagina = 4;
+
+    useEffect(() => {
+        // Função para buscar dados da API
+        const fetchData = async () => {
+            try {
+                const response = await fetch("https://api-conta-certa-production.up.railway.app/graphproximos/21?mes=11&ano=2023");
+                const data = await response.json();
+                // Organizar os dados por descrição
+                const dadosOrdenados = data.sort((a, b) => a.descricao.localeCompare(b.descricao));
+                setDadosDaTabela(dadosOrdenados);
+            } catch (error) {
+                console.error("Erro ao buscar dados da API", error);
+            }
+        };
+
+        // Chamar a função para buscar dados ao carregar o componente
+        fetchData();
+    }, []);
 
     const proximosDados = dadosDaTabela.slice(indiceAtual, indiceAtual + tamanhoPagina);
 
@@ -35,7 +48,7 @@ function Proximos() {
             <table className="table">
                 <thead>
                     <tr>
-                        <th colSpan="4" className="text-center">
+                        <th colSpan="3" className="text-center">
                             <h1 style={{
                                 fontSize: '14px',
                                 marginTop: '20px',
@@ -48,22 +61,21 @@ function Proximos() {
                 <tbody>
                     {proximosDados.map((linha, index) => (
                         <tr key={index}>
-                            <td>{linha.col1}</td>
-                            <td>{linha.col2}</td>
-                            <td><strong>{linha.col3}</strong></td>
-                            <td>{linha.col4}</td>
+                            <td>{linha.descricao}</td>
+                            <td>{format(new Date(linha.data), 'MMMM/yyyy', { locale: ptBR })}</td>
+                            <td><strong>R$ {linha.valor}</strong></td>
                         </tr>
                     ))}
                     <tr>
-                        <td colSpan="4">
+                        <td colSpan="3">
                             {indiceAtual > 0 && (
                                 <button className={styles.button} onClick={handleClickAnterior}>
-                                    <i className="bi bi-arrow-up-short"></i> 
+                                    <i className="bi bi-arrow-up-short"></i>
                                 </button>
                             )}
                             {indiceAtual + tamanhoPagina < dadosDaTabela.length && (
                                 <button className={styles.button} onClick={handleClickProximo}>
-                                    <i className="bi bi-arrow-down-short"></i> 
+                                    <i className="bi bi-arrow-down-short"></i>
                                 </button>
                             )}
                         </td>
