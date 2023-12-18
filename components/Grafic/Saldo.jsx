abe934da0d81345b917cac502607a4afdacfcb85
Pedro Menna <113869595+pedromenna@users.noreply.github.com>
2023-12-18 03:10:11 -0300

    correçãobugs

--- components/Grafic/Saldo.jsx
+++ components/Grafic/Saldo.jsx
@@ -1,96 +1,103 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Chart from 'chart.js/auto';
-import 'chartjs-plugin-datalabels';
+import React, { useState, useEffect } from "react";
+import { format } from "date-fns";
+import { ptBR } from 'date-fns/locale';
 import { useRouter } from 'next/router';
+import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import styles from './Ultimas.module.css';
 
-export default function Saldo() {
+function Proximos() {
+    const [indiceAtual, setIndiceAtual] = useState(0);
+    const [dadosDaTabela, setDadosDaTabela] = useState([]);
+    const router = useRouter();
+    const { usuarioId } = router.query;
 
-  const router = useRouter();
-  const usuarioId = router.query.usuarioId;
+    const tamanhoPagina = 4;
 
+    useEffect(() => {
+        // Função para buscar dados da API
+        const fetchData = async () => {
+            try {
+                if (!usuarioId) {
+                    // Se o usuárioId não estiver disponível, não fazemos a requisição
+                    return;
+                }
 
+                const response = await fetch(`https://api-conta-certa-production.up.railway.app/graphproximos/${usuarioId}?mes=11&ano=2023`);
+                
+                if (!response.ok) {
+                    console.error("Erro ao buscar dados da API:", response.statusText);
+                    return;
+                }
 
-
-  useEffect(() => {
-    if (usuarioId) {
-      console.log('Usuário ID:', usuarioId);
-      // Lógica adicional que depende de usuarioId
-    }})
-    
-
-
-
-  const chartRef = useRef();
-  const myChart = useRef(null);
-  const [entradaData, setEntradaData] = useState(0);
-  const [saidaData, setSaidaData] = useState(0);
-
-  useEffect(() => {
-    const fetchData = async (url, setData) => {
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        // Converte a string para número
-        setData(Number(data[0]?.total) || 0);
-      } catch (error) {
-        console.error(`Erro ao buscar dados da API ${url}:`, error);
-      }
-    };
-
-    fetchData(`https://api-conta-certa-production.up.railway.app/totalEntradas/${usuarioId}?mes=12&ano=2023`, setEntradaData);
-    fetchData(`https://api-conta-certa-production.up.railway.app/totalSaidas/${usuarioId}?mes=12&ano=2023`, setSaidaData);
-  }, []);
-
-  useEffect(() => {
-    const saldoData = entradaData - saidaData;
-
-    const ctx = chartRef.current.getContext('2d');
-
-    if (myChart.current) {
-      myChart.current.destroy();
-    }
-
-    const config = {
-      type: 'bar',
-      data: {
-        labels: ["Entradas", "Saídas", "Saldo"],
-        datasets: [{
-          axis: 'y',
-          data: [entradaData, saidaData, saldoData],
-          fill: false,
-          backgroundColor: [
-            'rgba(77, 228, 117, 1)',
-            'rgba(236, 84, 72, 1)',
-            'rgba(0, 223, 191, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        indexAxis: 'y',
-        plugins: {
-          legend: {
-            display: false
-          },
-          datalabels: {
-            display: true,
-            color: 'black',
-            anchor: 'end',
-            align: 'end',
-            font: {
-              weight: 'bold'
+                const data = await response.json();
+                // Organizar os dados por descrição
+                const dadosOrdenados = data.sort((a, b) => a.descricao.localeCompare(b.descricao));
+                setDadosDaTabela(dadosOrdenados);
+            } catch (error) {
+                console.error("Erro ao buscar dados da API", error);
             }
-          }
+        };
+
+        // Chamar a função para buscar dados ao carregar o componente
+        fetchData();
+    }, [usuarioId]);
+
+    const proximosDados = dadosDaTabela.slice(indiceAtual, indiceAtual + tamanhoPagina);
+
+    const handleClickProximo = () => {
+        if (indiceAtual + tamanhoPagina < dadosDaTabela.length) {
+            setIndiceAtual(indiceAtual + tamanhoPagina);
         }
-      }
     };
 
-    myChart.current = new Chart(ctx, config);
-  }, [entradaData, saidaData]);
+    const handleClickAnterior = () => {
+        if (indiceAtual - tamanhoPagina >= 0) {
+            setIndiceAtual(indiceAtual - tamanhoPagina);
+        }
+    };
 
-  return (
-    <div>
-      <canvas ref={chartRef} style={{ width: '400px', height: '300px', padding: '20px', fontWeight:'800' }}></canvas>
-    </div>
-  );
+    return (
+        <div className="container">
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th colSpan="3" className="text-center">
+                            <h1 style={{
+                                fontSize: '14px',
+                                marginTop: '20px',
+                                marginBottom: '20px',
+                                fontWeight: '800',
+                            }}>Próximos vencimentos</h1>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {proximosDados.map((linha, index) => (
+                        <tr key={index}>
+                            <td>{linha.descricao.charAt(0).toUpperCase() + linha.descricao.slice(1)}</td>
+                            <td>{format(new Date(linha.data), 'MMMM/yyyy', { locale: ptBR })}</td>
+                            <td><strong>R$ {linha.valor}</strong></td>
+                        </tr>
+                    ))}
+                    <tr>
+                        <td colSpan="3">
+                            {indiceAtual > 0 && (
+                                <button className={styles.button} onClick={handleClickAnterior}>
+                                    <i className="bi bi-arrow-up-short"></i>
+                                </button>
+                            )}
+                            {indiceAtual + tamanhoPagina < dadosDaTabela.length && (
+                                <button className={styles.button} onClick={handleClickProximo}>
+                                    <i className="bi bi-arrow-down-short"></i>
+                                </button>
+                            )}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 }
+
+export default Proximos;
